c67cabaafbed68e24168c328123436e71267d219
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-06-24 19:09:22 +0430

    target files removed

--- bank-b-service/target/classes/test-schema-mysql.sql
+++
@@ -1,8 +0,0 @@
-;
--- CREATE TABLE TRANSACTION_BANK_B (
---   id INT AUTO_INCREMENT PRIMARY KEY,
---   account_id BIGINT,
---   amount BIGINT,
---   created_at TIMESTAMP,
---   updated_at TIMESTAMP
--- );
