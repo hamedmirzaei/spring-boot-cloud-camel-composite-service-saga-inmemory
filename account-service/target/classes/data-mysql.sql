2e9dc0051c2d5f1f9e2ea801c068eda7b0199944
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-07-01 09:52:17 +0430

    gitignore added

--- account-service/target/classes/data-mysql.sql
+++
@@ -1,5 +0,0 @@
-;
--- INSERT INTO CUSTOMER(id, cif_number, first_name, last_name, created_at, updated_at) values (1, 1453223564, 'Hamed', 'Mirzaei', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- INSERT INTO CUSTOMER(id, cif_number, first_name, last_name, created_at, updated_at) values (2, 4154674165, 'Ali', 'Mirzaei', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- INSERT INTO ACCOUNT(id, account_number, customer_id, balance, status, created_at, updated_at) values (1, '003544343', 1, 1000000, 'IDEAL', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- INSERT INTO ACCOUNT(id, account_number, customer_id, balance, status, created_at, updated_at) values (2, '000453158', 2, 2000000, 'IDEAL', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- transaction-service/target/classes/test-schema-mysql.sql
+++
@@ -1,8 +0,0 @@
-;
--- CREATE TABLE TRANSACTION (
---   id INT AUTO_INCREMENT PRIMARY KEY,
---   account_id BIGINT,
---   amount BIGINT,
---   created_at TIMESTAMP,
---   updated_at TIMESTAMP
--- );
