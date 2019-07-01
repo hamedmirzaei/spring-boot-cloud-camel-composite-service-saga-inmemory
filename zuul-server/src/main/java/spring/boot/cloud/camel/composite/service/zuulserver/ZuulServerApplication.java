15ef6adbbc626646cab16103c23e4636af1291a7
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-07-01 09:49:57 +0430

    zuul server is added for load balancing

---
+++ zuul-server/src/main/java/spring/boot/cloud/camel/composite/service/zuulserver/ZuulServerApplication.java
@@ -0,0 +1,21 @@
+package spring.boot.cloud.camel.composite.service.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableZuulServer
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+
+}
